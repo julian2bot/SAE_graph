0c875b4bcc21db8bbc84725f5e243bb3f9ef5931
julian2bot <marquesjulian26@gmail.com>
2024-05-17 14:09:29 +0200

    refonte github car fail, fichier clean

--- src/APIGraph.py
+++ src/APIGraph.py
@@ -1,11 +1,16 @@
 import json
 import os
-
 import networkx as nx
-
+import itertools
+import re
 import matplotlib.pyplot as plt
+import time
+import cProfile
+from collections import deque
 
+t_start = time.perf_counter()
 
+# LE BON
 def json_vers_nx(file_path, affiche=False):
 
     G = nx.Graph()
@@ -26,6 +31,8 @@ def json_vers_nx(file_path, affiche=False):
 
         return G
 
+
+  
 # affichage du graphe :
 def affichage_graph(graph_films):
     pos = nx.spring_layout(graph_films)
@@ -33,7 +40,6 @@ def affichage_graph(graph_films):
     nx.draw(graph_films, pos, with_labels=True)
     fig.savefig("G12.png")
     plt.show()
-
 
 def save_graph(graph_films):
     """Affiche le graphique passé en paramètre grâce à matplotlib et le sauvegarde sous forme d'image.
@@ -54,29 +60,296 @@ def save_graph(graph_films):
     return str(os.path.abspath(nameimg))
 
 
+def collaborateurs_communs(G, u, v):
+    if u not in G or v not in G:
+        return None
+
+    voisins_u = set(G.neighbors(u))
+    voisins_v = set(G.neighbors(v))
+    return voisins_u.intersection(voisins_v)
+
+
+def collaborateurs_proches(G,u,k):
+    """Fonction renvoyant l'ensemble des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: le sommet de départ
+        k: la distance depuis u
+    """
+    if u not in G.nodes:
+        print(u,"est un illustre inconnu")
+        return None
+    collaborateurs = set()
+    collaborateurs.add(u)
+    # print(collaborateurs)
+    for i in range(k):
+        collaborateurs_directs = set()
+        for c in collaborateurs:
+            for voisin in G.adj[c]:
+                if voisin not in collaborateurs:
+                    collaborateurs_directs.add(voisin)
+        collaborateurs = collaborateurs.union(collaborateurs_directs)
+    return collaborateurs
+
+
+def est_proche(G,u,v,k=1):
+    if u not in G or v not in G or k<=-1:
+        return None
+    return v in collaborateurs_proches(G,u,k) 
+
+
+def distance_naive(G, u, v):
+    if u not in G.nodes or v not in G.nodes:
+        return -1
+    distance = 0
+    
+    while distance <100:
+        if est_proche(G, u, v, k=distance):
+            return distance
+        distance += 1
+    return -1
+
+
+# Cette fonction distance utilise BFS pour trouver la distance entre deux acteurs u et v dans un graphe G. Sa complexité est O(V + E), ce qui est plus efficace que l'approche naïve précédente.
+def distance(G, u, v):
+    # retorune distance dsitra 
+    try:
+        return nx.shortest_path_length(G,u, v) # a TEST oui => 0.10
+    except: return -1
+
+        # return nx.dijkstra_path_length(G,u, v) # ok mais peut mieux faire
+        # return nx.single_source_dijkstra_path_length(G, u,v) # non 
+        # return nx.floyd_warshall_numpy(G, u,v) # mais non
+        # return nx.algorithms.shortest_paths.astar(G, u,v)# a TEST => 0.09 mais non
+        # return nx.algorithms.shortest_paths.dense (G, u,v)# a TEST => 1.32 mais non
+        # return nx.algorithms.shortest_paths.generic (G, u,v)# a TEST => 1.16 mais non
+        # return nx.algorithms.shortest_paths.unweighted(G, u,v)# a TEST => 1.26 mais non
+        # return nx.algorithms.shortest_paths.unweighted(G, u,v)# a TEST => 1.15 mais non
+        # return nx.algorithms.shortest_paths.unweighted.single_source_shortest_path_length(G, u,v)# a TEST => 0.09 mais non 
+
+        # return int(nx.all_shortest_paths(G, source=u, target=v))
+
+    # visited = set()
+    # queue = [(u, 0)]
+    # while queue:
+    #     node, dist = queue.pop(0)
+    #     if node == v:
+    #         return dist
+    #     visited.add(node)
+    #     for neighbor in G.neighbors(node):
+    #         if neighbor not in visited:
+    #             queue.append((neighbor, dist + 1))
+    # return -1 
 
 
 
+def centralite(G, u):
+    """ renvoie une distance entre u et le bord du graph
+
+    Args:
+        G: le graphe
+        u: le sommet de départ de l'acteur
+    Returns:
+        int : la centralité depuis u vers v
+    """
+    if u not in G:
+        return None
+    shortest_paths = nx.single_source_dijkstra_path_length(G, u)
+    centralite_u = max(shortest_paths.values())
+    return centralite_u
+    # or
+    # return max(nx.single_source_dijkstra_path_length(G, u).values())
 
 
 
+def bfs_max_distance(G, source):
+    if source not in G:
+        return None
+    visited = set()
+    queue = deque([(source, 0)])  # Queue pour BFS, avec la distance de départ 0
+    max_distance = 0
+
+    while queue:
+        node, distance = queue.popleft()
+        visited.add(node)
+        max_distance = max(max_distance, distance)
+
+        for neighbor in G.neighbors(node):
+            if neighbor not in visited:
+                queue.append((neighbor, distance + 1))
+
+    return max_distance
+
+def eloignement_max(G):
+    max_distance = 0
+    for node in G.nodes():
+        distance_from_node = bfs_max_distance(G, node)
+        max_distance = max(max_distance, distance_from_node)
+    
+    return max_distance
+
+# def parcours_largeur(graphe, depart):
+#     if depart not in graphe:
+#         return None
+    
+#     sommet_a_explorer = [depart]
+#     deja_vu = {depart}
+
+#     while (len(sommet_a_explorer)>0):
+#         noeud_courant = sommet_a_explorer.pop(0)
+#         print(noeud_courant)
+#         for noeud in graphe[noeud_courant]:
+#             if noeud not in deja_vu:
+#                 deja_vu.add(noeud_courant)
+#                 sommet_a_explorer.append(noeud)
 
 
+def arthus_sauce(G):
+    # networkx.algorithms.shortest_paths.generic.shortest_path RAISE
+    # if G.number_of_nodes() == 0:
+    #     return None
+
+    graphe = G
+    start = list(G.nodes.keys())[0]
+    depart = list(G[start].keys())[0]
+    sommet_a_explorer = [depart]
+    deja_vu = {depart , start}
+    teh_end = [0,depart]
+    while (len(sommet_a_explorer)>0):
+        noeud_courant = sommet_a_explorer.pop(0)
+        dist = distance(G,depart,noeud_courant)
+        if dist > teh_end[0]:
+            teh_end = [dist,noeud_courant]
+        # print(noeud_courant)
+        for noeud in graphe[noeud_courant]:
+            if noeud not in deja_vu:
+                deja_vu.add(noeud_courant)
+                sommet_a_explorer.append(noeud)
+
+    print("1er/2 DFS")
+    # DFS depart OK
+    depart = teh_end[1]
+    sommet_a_explorer = [depart]
+    deja_vu = {depart}
+    teh_end = [0,depart]
+    while (len(sommet_a_explorer)>0):
+        noeud_courant = sommet_a_explorer.pop(0)
+        if distance(G,depart,noeud_courant) > teh_end[0]:
+            teh_end = [distance(G,depart,noeud_courant),noeud_courant]
+        # print(noeud_courant)
+        for noeud in graphe[noeud_courant]:
+            if noeud not in deja_vu:
+                deja_vu.add(noeud_courant)
+                sommet_a_explorer.append(noeud)
+    return (depart,teh_end[1], distance(G,depart,teh_end[1]))
 
 
+def eloignement_max(G):
+    return arthus_sauce(G)
+
+def cheminer(G,u,v):
+    # return nx.dag_longest_path(G,u,v)
+    """
+    On suppose que l'algorithme de DFS a été effectué sur G depuis un sommet quelconque r
+    """
+    import heapq
+    if u not in G or v not in G:
+        return None
+
+    # si il y a pas de chemin ba va te fiare
+    distances = {}
+    previous_nodes = {}
+    heap = [(0, u)]  # (distance jusqu'au nœud, nœud)
+
+    while heap:
+        current_distance, current_node = heapq.heappop(heap)
+        # print(current_distance,current_node)
+    
+        # Vérifier si le nœud actuel est le nœud d'arrivée
+        if current_node == v:
+            path = []
+            # print("entrain de faire chemin back")
+            previous_nodes[u] = None
+            while current_node is not None:
+                path.append(current_node)
+                print(path)
+                current_node = previous_nodes.get(current_node)
+            # print("END",path)
+            return path[::-1]  # Retourne le chemin dans l'ordre correct
+        print("not end")
+        # Parcourir les voisins du nœud actuel
+        for neighbor in G[current_node]:
+            distance = current_distance + 1
+            # Mettre à jour la distance si elle est plus courte que la précédente
+            if neighbor not in distances or distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous_nodes[neighbor] = current_node
+                heapq.heappush(heap, (distance, neighbor))
+    
+
+def centre_hollywood(G,sauce):
+    # if sauce == None or G.number_of_nodes():
+    #     return None
+
+    depart , end , distance = sauce
+    # depart , end , distance = nx.algorithms.shortest_paths.generic.shortest_path(G,)
+
+    print(" depart ",depart,  " end ",end,  " distance ",distance)
+    # ch = nx.algorithms.shortest_paths.dense.reconstruct_path( depart, end,G)
+    # ch =nx.algorithms.shortest_paths.generic.shortest_path(G,depart, end)
+    ch = cheminer(G,depart,end)
+
+    print("ch>>>>>",ch)
+    return ch[len(ch) // 2]
 
 
+def centralite_groupe(G,S):
+    if S not in G:
+        return None
+    return max([centralite(G,u) for u in S])
 
+def lesprints():
+    # fichier = './data/dataTests.txt'
+    fichier ="./data/data.txt"
+    graph_films = json_vers_nx(fichier, True)
+# print(graph_films)
+# Afficher quelques informations sur le graphe
+# print("Nombre de nœuds (personnes) :", graph_films.number_of_nodes())
+# print("Nombre d'arêtes (relations) :", graph_films.number_of_edges())
+    # affichage_graph(graph_films)
+    # print(save_graph(graph_films))
+    print(graph_films)
+    
+    # print(centre_hollywood(graph_films,eloignement_max(graph_films))) 
+    
+    print("colab: ", collaborateurs_communs(graph_films, "Paul Reubens", "Julian Marques"))  # Output: {'Carol Kane'}
+    print("colab proche: ", collaborateurs_proches(graph_films,"Julian Marques",1))
+    print("colab proche: ", collaborateurs_proches(graph_films,"Julian Marques",2))
+    print("EST proche: ", est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",1) )
+    print("EST proche: ", est_proche(graph_films,"Julian Marques", "Carol Kane",1))
+    print("distance_naive:", distance_naive(graph_films,"Julian Marques","Carol Kane") )
+    print("distance_naive:", distance_naive(graph_films,"Julian Marques","Julian Marques") )
+    print("distance_naive:", distance_naive(graph_films,"Julian Marques","Anthony Michael Hall") )
+    print("distance_naive:", distance_naive(graph_films,"Julian Marques","Goldie Hawn") )
+    print("distance:", distance(graph_films,"Julian Marques","Carol Kane") )
+    print("distance:", distance(graph_films,"Julian Marques","Julian Marques") )
+    print("distance:", distance(graph_films,"Julian Marques","Anthony Michael Hall") )
+    print("distance:", distance(graph_films,"Julian Marques","Goldie Hawn") )
+    print("centralite:", centralite(graph_films,"Julian Marques") ) # 3
+    print("centralite:", centralite(graph_films,"Goldie Hawn") ) # 3
+    print("centralite:", centralite(graph_films,"Carol Kane") ) # 2
+    # # #Gcentre= graph_films
+    # arthus_sauce1 = arthus_sauce(graph_films)
+    # print("centre_hollywood:", centre_hollywood(graph_films,arthus_sauce1) ) 
 
+    # print("eloignement_max:", eloignement_max(graph_films) )
+    # print("eloignement_max:", arthus_sauce1 )
+    # liste = ["Carol Kane","Paul Reubens"]
+    # print("centralite_groupe:", centralite_groupe(graph_films,liste) )
 
+    # print("time taken",time.perf_counter()-t_start,"s")
 
-
-
-
-
-
-
-
-
-
-
+# cProfile.run("lesprints()")
+lesprints()
+print("time taken",time.perf_counter()-t_start,"s")
--- src/lesTest.py
+++ src/lesTest.py
@@ -1,66 +1,66 @@
-import graphdocpython as graphdocpython
+import APIGraph as API
 import networkx as nx
-graph_films = graphdocpython.json_vers_nx("./data/dataTests.txt")
+graph_films = APIGraph.json_vers_nx("./data/dataTests.txt")
 
 # # Q1
 def test_json_vers_nx():
-    graphdocpython.affichage_graph(graph_films)
+    API.affichage_graph(graph_films)
 # # Q2
 def test_collaborateurs_communs():
-    assert graphdocpython.collaborateurs_communs(graph_films, "Paul Reubens", "Julian Marques") == {'Carol Kane'}
-    assert graphdocpython.collaborateurs_communs(graph_films, "Jill Whitlow", "Kevin Nealon") == {'Wallace Langham'}
-    assert graphdocpython.collaborateurs_communs(graph_films, "robert", "Julian Marques") == None
-    assert graphdocpython.collaborateurs_communs(graph_films, "Paul Reubens", "robert") == None
+    assert API.collaborateurs_communs(graph_films, "Paul Reubens", "Julian Marques") == {'Carol Kane'}
+    assert API.collaborateurs_communs(graph_films, "Jill Whitlow", "Kevin Nealon") == {'Wallace Langham'}
+    assert API.collaborateurs_communs(graph_films, "robert", "Julian Marques") == None
+    assert API.collaborateurs_communs(graph_films, "Paul Reubens", "robert") == None
 
 
 # # Q3
 def test_collaborateurs_proches():
-    assert graphdocpython.collaborateurs_proches(graph_films,"Julian Marques",1) == {'Carol Kane', 'Julian Marques'}
-    assert graphdocpython.collaborateurs_proches(graph_films,"Julian Marques",2) == {'Judge Reinhold', 'Candice Azzara', 'Kaye Ballard', 'Paul Reubens', 'Eileen Brennan', 'Teri Landrum', 'Eve Arden', 'Carol Kane', 'David L. Lander', "Donald O'Connor", 'Tab Hunter', 'Debralee Scott', 'Tom Smothers', 'Marc McClure', 'Julian Marques', 'Phil Hartman'}
-    assert graphdocpython.collaborateurs_proches(graph_films,"rober",2) == None
+    assert API.collaborateurs_proches(graph_films,"Julian Marques",1) == {'Carol Kane', 'Julian Marques'}
+    assert API.collaborateurs_proches(graph_films,"Julian Marques",2) == {'Judge Reinhold', 'Candice Azzara', 'Kaye Ballard', 'Paul Reubens', 'Eileen Brennan', 'Teri Landrum', 'Eve Arden', 'Carol Kane', 'David L. Lander', "Donald O'Connor", 'Tab Hunter', 'Debralee Scott', 'Tom Smothers', 'Marc McClure', 'Julian Marques', 'Phil Hartman'}
+    assert API.collaborateurs_proches(graph_films,"rober",2) == None
 
 def test_est_proche():
-    assert graphdocpython.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",1) == False
-    assert graphdocpython.est_proche(graph_films,"Julian Marques", "Carol Kane",2) == True
-    assert graphdocpython.est_proche(graph_films,"robert", "Carol Kane",2) == None
-    assert graphdocpython.est_proche(graph_films,"Julian Marques", "robert",2) == None
-    assert graphdocpython.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",-1) == None
-    assert graphdocpython.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",-101) == None
+    assert API.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",1) == False
+    assert API.est_proche(graph_films,"Julian Marques", "Carol Kane",2) == True
+    assert API.est_proche(graph_films,"robert", "Carol Kane",2) == None
+    assert API.est_proche(graph_films,"Julian Marques", "robert",2) == None
+    assert API.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",-1) == None
+    assert API.est_proche(graph_films,"Julian Marques", "Anthony Michael Hall",-101) == None
     
 def test_distance_naive():
 
 
-    assert graphdocpython.distance_naive(graph_films,"Julian Marques","Carol Kane")  == 1
-    assert graphdocpython.distance_naive(graph_films,"Julian Marques","Julian Marques")  == 0
-    assert graphdocpython.distance_naive(graph_films,"Julian Marques","Anthony Michael Hall" ) == -1 
-    assert graphdocpython.distance_naive(graph_films,"Julian Marques","Goldie Hawn")  == 3
-    assert graphdocpython.distance_naive(graph_films,"robert","Goldie Hawn")  == -1
-    assert graphdocpython.distance_naive(graph_films,"Julian Marques","robert")  == -1
+    assert API.distance_naive(graph_films,"Julian Marques","Carol Kane")  == 1
+    assert API.distance_naive(graph_films,"Julian Marques","Julian Marques")  == 0
+    assert API.distance_naive(graph_films,"Julian Marques","Anthony Michael Hall" ) == -1 
+    assert API.distance_naive(graph_films,"Julian Marques","Goldie Hawn")  == 3
+    assert API.distance_naive(graph_films,"robert","Goldie Hawn")  == -1
+    assert API.distance_naive(graph_films,"Julian Marques","robert")  == -1
 
 
 def test_distance():
 
-    assert graphdocpython.distance(graph_films,"Julian Marques","Carol Kane")  == 1
-    assert graphdocpython.distance(graph_films,"Julian Marques","Julian Marques")  == 0
-    assert graphdocpython.distance(graph_films,"Julian Marques","Anthony Michael Hall" ) == -1 
-    assert graphdocpython.distance(graph_films,"Julian Marques","Goldie Hawn")  == 3
-    assert graphdocpython.distance(graph_films,"robert","Goldie Hawn")  == -1
-    assert graphdocpython.distance(graph_films,"Julian Marques","robert")  == -1
+    assert API.distance(graph_films,"Julian Marques","Carol Kane")  == 1
+    assert API.distance(graph_films,"Julian Marques","Julian Marques")  == 0
+    assert API.distance(graph_films,"Julian Marques","Anthony Michael Hall" ) == -1 
+    assert API.distance(graph_films,"Julian Marques","Goldie Hawn")  == 3
+    assert API.distance(graph_films,"robert","Goldie Hawn")  == -1
+    assert API.distance(graph_films,"Julian Marques","robert")  == -1
 
 # # Q4
 def test_centralite():
 
-    assert graphdocpython.centralite(graph_films,"Julian Marques") ==  3
-    assert graphdocpython.centralite(graph_films,"Goldie Hawn") ==  3
-    assert graphdocpython.centralite(graph_films,"Carol Kane") ==  2
-    assert graphdocpython.centralite(graph_films,"robert") ==  None
-    assert graphdocpython.centralite(nx.Graph(),"robert") ==  None
+    assert API.centralite(graph_films,"Julian Marques") ==  3
+    assert API.centralite(graph_films,"Goldie Hawn") ==  3
+    assert API.centralite(graph_films,"Carol Kane") ==  2
+    assert API.centralite(graph_films,"robert") ==  None
+    assert API.centralite(nx.Graph(),"robert") ==  None
 
 def test_centre_hollywood():
 
-    assert graphdocpython.centre_hollywood(graph_films, graphdocpython.eloignement_max(graph_films)) ==  'Eileen Brennan'
-    assert graphdocpython.centre_hollywood(nx.Graph(), graphdocpython.eloignement_max(graph_films)) ==  None
-    assert graphdocpython.centre_hollywood(graph_films, None) ==  None
+    assert API.centre_hollywood(graph_films, API.eloignement_max(graph_films)) ==  'Eileen Brennan'
+    assert API.centre_hollywood(nx.Graph(), API.eloignement_max(graph_films)) ==  None
+    assert API.centre_hollywood(graph_films, None) ==  None
     
 # # Q5
 # def test_eloignement_max():
