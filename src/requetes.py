776a9932b56f23e91fd06ae51b23554cc423a44e
Julian <160622256+julian2bot@users.noreply.github.com>
2024-05-25 19:32:21 +0200

    Update requetes.py dicstring v2

--- src/requetes.py
+++ src/requetes.py
@@ -10,6 +10,15 @@ from collections import deque
 
 # Q1
 def json_vers_nx(file_path2, affiche=False):
+    """Transforme un fichier en graphe
+
+    Parametres:
+        file_path2 (str): chemin vers le document à transformer en graphe
+        affiche (bool, optionel): Demande si il faut affiche le graphe False par défaut.
+
+    Resultat:
+        nx.Graph: le graphe obtenu
+    """
     print('file', file_path2)
     file_path = file_path2.strip("{").strip("}")
 
@@ -32,6 +41,13 @@ def json_vers_nx(file_path2, affiche=False):
 
 # affichage du graphe :
 def affichage_graph(graph_films, label = False):
+    """Affiche le graphe avec ou sans les labels des sommets.
+
+    Parametres:
+        graph_films (nx.Graph): le graphe
+        label (bool, optionel): Demande si il faut afficher les labels. False par defaut.
+    """
+
     pos = nx.spring_layout(graph_films)
     fig = plt.figure()
     nx.draw(graph_films, pos, with_labels=True)
@@ -46,9 +62,10 @@ def affichage_graph(graph_films, label = False):
 def save_graph(graph_films, cheminnameimg= None):
     """Affiche le graphique passé en paramètre grâce à matplotlib et le sauvegarde sous forme d'image.
 
-    Args:
+    Parametres:
         graph_films (nx.Graph): Le graphique à afficher
-    Returns:
+        chemin_name_img (str):Le nom sous lequel sera enregistré le graphe. Par défaut, graph.png
+    Resultat:
         str: Le chemin absolu de l'image enregistrée
     """
     if cheminnameimg is None:
@@ -68,6 +85,16 @@ def save_graph(graph_films, cheminnameimg= None):
 
 # Q2
 def collaborateurs_communs(G, u, v):
+    """Fonction qui trouve tous les sommets possédants un chemin vers les sommets u et v
+
+    Parametres:
+        G (nx.Graph): le graphe
+        u (Any): un premier sommet
+        v (Any): un deuxième sommet
+
+    Resultat:
+        set: l'ensemble des sommets possédants un chemin entre les deux sommets
+    """
     if u not in G or v not in G:
         return None
 
@@ -84,6 +111,9 @@ def collaborateurs_proches(G,u,k):
         G: le graphe
         u: le sommet de départ
         k: la distance depuis u
+    
+    Resultat:
+        set: l'ensemble des sommets se situants à une distance au plus k du sommet u    
     """
     if u not in G.nodes:
         print(u,"est un illustre inconnu")
@@ -102,11 +132,32 @@ def collaborateurs_proches(G,u,k):
 
 
 def est_proche(G,u,v,k=1):
+    """Vérifie si le sommet v se situe à une distance au plus k du sommet u
+
+    Parametres:
+        G (nx.Graph): le graphe
+        u (Any): le premier sommet
+        v (Any): le deuxieme sommet
+        k (int, optional): _description_. Defaults to 1.
+
+    Resultat:
+        bool: _description_
+    """
     if u not in G or v not in G or k<=-1:
         return None
     return v in collaborateurs_proches(G,u,k) 
 
 def distance_naive(G, u, v):
+    """Fonction qui peut retourner la distance entre u et v
+
+    Parametres:
+        G (nx.Graph): le graphe
+        u (Any): premier sommet
+        v (Any): deuxieme sommet
+
+    Resultat:
+        int: distance potentiel entre les deux sommets. -1 si l'un des sommets ne fait pas parti du graphe ou queles sommets se situe à une distance supérieur à 100 ou que les sommets ne sont pas dans la meme composante connexe
+    """    
     if u not in G.nodes or v not in G.nodes:
         return -1
     distance = 0
@@ -118,6 +169,16 @@ def distance_naive(G, u, v):
     return -1
 
 def distance(G, u, v):
+    """Fonction qui retourne la distance la plus petite entre les sommets u et v
+
+    Parametres:
+        G (nx.Graph): le graphe
+        u (Any): premier sommet
+        v (Any): deuxième sommet
+
+    Resultat:
+        int: la distance entre u et v, -1 si il n'existe pas de chemin.
+    """
     try:
         return nx.shortest_path_length(G,u, v)
     except: 
@@ -127,6 +188,15 @@ def distance(G, u, v):
 
 # Q4
 def centralite(G,u):
+    """Calcule la centralite du sommet u
+
+    Parametres:
+        G (nx.Graph): le  graphe
+        u (Any): un sommet
+
+    Resultat:
+        int: la centralite du sommet u
+    """    
     try: 
     
         def val(elem):
@@ -139,7 +209,14 @@ def centralite(G,u):
         return None
 
 def centre_hollywood(G):
-    
+    """Fonction qui trouve le centre du graphe
+
+    Parametres:
+        G (nx.Graph): le graphe
+
+    Resultat:
+        Any: le sommet au centre du graphe
+    """    
     # ch = nx.single_source_dijkstra_path(G,centralite(G, centralite(G, list(G.nodes)[0])[0])[0])
     def val(elem):
         return len(elem[1])
@@ -150,24 +227,42 @@ def centre_hollywood(G):
 
 # Q5
 def eloignement_max(G):
+    """Fonction qui retourne la longueur de la chaine la plus longue du graphe
+
+    Parametres:
+        G (nx.Graph): le graphe
+
+    Resultat:
+        int: la longueur de la chaine la plus longue du graphe
+    """
     return centralite(G, centralite(G, list(G.nodes)[0])[0])
 
 # BONUS
 def centralite_groupe(G,S):
-    # if S not in G:
-    #     return None
-    # return max([centralite(G,u) for u in S])
+    """Fonction renvoyant le sommet au centre du groupe 
+
+    Parametres:
+        G (nx.Graph): le graphe
+        S (set): ensemble de sommets
+
+    Resultat:
+        Any: le sommet au centre du groupe
+    """
+    # return max([centralite(G,u) for u in S]) # mais la version de dessous est mieux car si un acteur de S n'existe pas ca crash donc:
     return min([centralite(G, u) for u in S if centralite(G, u) is not None], default=None)
 
 
 # BONUS collab proche to graph
 def collaborateurs_proches_Graph(G,u,k):
-    """Fonction renvoyant l'ensemble des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+    """Fonction renvoyant un sous-graphe des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
     
     Parametres:
-        G: le graphe
-        u: le sommet de départ
-        k: la distance depuis u
+        G(nx.Graph): le graphe
+        u(Any): le sommet de départ
+        k(int): la distance depuis u
+    
+    Resultat:
+        nx.Graph: un sous graphe de G
     """
     Graph = nx.Graph()
 
